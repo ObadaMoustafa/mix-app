4aa8ac0c19110e8bad5d2b2bab364fd696d11ae4
Obada <obadamoustafa@gmail.com>
2021-11-22 18:33:17 +0100

    ready to deploy

--- src/context/ConverterContext.js
+++ src/context/ConverterContext.js
@@ -2,36 +2,6 @@ import React, { createContext, useState } from 'react'
 export const ConverterContext = createContext();
 
 export const ConverterContextProv = ({ children }) => {
-    const dummyHistory = [
-        {
-            from: "EGP",
-            to: "USD",
-            amount: 55,
-            result: 1500,
-            id: "54154asd5asd4sd"
-        },
-        {
-            from: "EGP",
-            to: "KRW",
-            amount: 55,
-            result: 23501.5451,
-            id: "54154asd974as54sd"
-        },
-        {
-            from: "EGP",
-            to: "EUR",
-            amount: 55,
-            result: 1200.354,
-            id: "54154as4das54dgh54sd"
-        },
-        {
-            from: "EGP",
-            to: "AWG",
-            amount: 55,
-            result: 451.25,
-            id: "54154asd5qc564sd"
-        },
-    ]
 
     const [selectedCurrencyFrom, setSelectedCurrencyFrom] = useState("EUR");
     const [selectedCurrencyTo, setSelectedCurrencyTo] = useState("USD");
